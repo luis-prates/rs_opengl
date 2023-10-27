90c6f82d32d031780daab320559b349d2a1323d6
Luis Prates <lprates@student.42lisboa.com>
2023-10-27 22:58:18 +0100

    Transformations chapter done

---
+++ src/_1_getting_started/_5_3_transforms_ex2.rs
@@ -0,0 +1,270 @@
+extern crate glfw;
+
+use cgmath::{Matrix4, vec3, Rad};
+use cgmath::prelude::*;
+use gl::types::{GLfloat, GLsizeiptr};
+
+use crate::shader;
+
+use self::glfw::{Action, Context, Key};
+
+extern crate gl;
+use self::gl::types::*;
+
+use std::{mem, ptr};
+use std::os::raw::c_void;
+use std::sync::mpsc::Receiver;
+use std::path::Path;
+use std::ffi::{CStr, CString};
+
+use shader::Shader;
+
+extern crate image;
+use image::GenericImage;
+
+// settings
+const SCR_WIDTH: u32 = 800;
+const SCR_HEIGHT: u32 = 600;
+
+pub fn main_1_5_3() {
+    // glfw: initialize and configure
+    // ------------------------------
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
+    #[cfg(target_os = "macos")]
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+
+    // glfw window creation
+    // --------------------
+    let (mut window, events) = glfw
+        .create_window(
+            SCR_WIDTH,
+            SCR_HEIGHT,
+            "LearnOpenGL",
+            glfw::WindowMode::Windowed,
+        )
+        .expect("Failed to create GLFW window");
+
+    window.make_current();
+    window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+
+    // gl: load all OpenGL function pointers
+    // ---------------------------------------
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+	let (our_shader, vbo, vao, ebo, texture1, texture2) = unsafe {
+		let our_shader = Shader::new(
+			"src/_1_getting_started/shaders/5.1.transform.vs", 
+			"src/_1_getting_started/shaders/4.6.texture_shader_combined_ex4.fs"
+		);
+
+		// make vertices
+		let vertices: [f32; 32] = [
+			// positions       // colors        // texture coords
+			0.5,  0.5, 0.0,   1.0, 0.0, 0.0,   1.0, 1.0, // top right
+			0.5, -0.5, 0.0,   0.0, 1.0, 0.0,   1.0, 0.0, // bottom right
+		   -0.5, -0.5, 0.0,   0.0, 0.0, 1.0,   0.0, 0.0, // bottom left
+		   -0.5,  0.5, 0.0,   1.0, 1.0, 0.0,   0.0, 1.0  // top left
+		];
+
+		let indices = [
+            0, 1, 3,  // first Triangle
+            1, 2, 3   // second Triangle
+        ];
+	
+		let (mut vbo, mut vao, mut ebo) = (0, 0, 0);
+	
+		// we can also generate multiple VAOs or buffers at the same time
+		gl::GenVertexArrays(1, &mut vao);
+		gl::GenBuffers(1, &mut vbo);
+		gl::GenBuffers(1, &mut ebo);
+
+		// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+		// first triangle setup
+        // --------------------
+		gl::BindVertexArray(vao);
+
+		gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+		gl::BufferData(
+			gl::ARRAY_BUFFER,
+			(vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+			&vertices[0] as *const f32 as *const c_void,
+			gl::STATIC_DRAW,
+		);
+
+		gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+        gl::BufferData(
+			gl::ELEMENT_ARRAY_BUFFER,
+            (indices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            &indices[0] as *const i32 as *const c_void,
+            gl::STATIC_DRAW
+		);
+
+		let stride = 8 * mem::size_of::<GLfloat>() as GLsizei;
+		// position attribute
+		gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, stride, ptr::null());
+		gl::EnableVertexAttribArray(0);
+
+		// color attribute
+		gl::VertexAttribPointer(1, 3, gl::FLOAT, gl::FALSE, stride, (3 * mem::size_of::<GLfloat>()) as *const c_void);
+		gl::EnableVertexAttribArray(1);
+
+		// texture coord attribute
+		gl::VertexAttribPointer(2, 2, gl::FLOAT, gl::FALSE, stride, (6 * mem::size_of::<GLfloat>()) as *const c_void);
+		gl::EnableVertexAttribArray(2);
+
+		// uncomment this call to draw in wireframe polygons.
+        // gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
+
+		let mut texture1 = 0;
+		gl::GenTextures(1, &mut texture1);
+		gl::BindTexture(gl::TEXTURE_2D, texture1); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+		// set the texture wrapping parameters
+		gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
+		gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+		// set the texture filtering parameters
+		gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+		gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+		// load image, create texture and generate mipmaps
+		let img = image::open(&Path::new("resources/textures/container.jpg")).expect("Failed to load texture");
+		let data = img.raw_pixels();
+		gl::TexImage2D(
+			gl::TEXTURE_2D,
+			0,
+			gl::RGB as i32,
+			img.width() as i32,
+			img.height() as i32,
+			0,
+			gl::RGB,
+			gl::UNSIGNED_BYTE,
+			&data[0] as *const u8 as *const c_void
+		);
+		gl::GenerateMipmap(gl::TEXTURE_2D);
+
+		let mut texture2 = 0;
+		gl::GenTextures(1, &mut texture2);
+		gl::BindTexture(gl::TEXTURE_2D, texture2); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+		// set the texture wrapping parameters
+		gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
+		gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+		// set the texture filtering parameters
+		gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+		gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+		// load image, create texture and generate mipmaps
+		let img = image::open(&Path::new("resources/textures/awesomeface.png")).expect("Failed to load texture");
+		let img = img.flipv();
+		let data = img.raw_pixels();
+		gl::TexImage2D(
+			gl::TEXTURE_2D,
+			0,
+			gl::RGB as i32,
+			img.width() as i32,
+			img.height() as i32,
+			0,
+			gl::RGBA,
+			gl::UNSIGNED_BYTE,
+			&data[0] as *const u8 as *const c_void
+		);
+		gl::GenerateMipmap(gl::TEXTURE_2D);
+
+
+		(our_shader, vbo, vao, ebo, texture1, texture2)
+	};
+
+
+
+    // render loop
+	unsafe { 
+		our_shader.use_program();
+		our_shader.set_int(CStr::from_bytes_with_nul(b"texture1\0").unwrap(), 0);
+		our_shader.set_int(CStr::from_bytes_with_nul(b"texture2\0").unwrap(), 1);
+	}
+
+	let mut mix_value = 0.5;
+    // -----------
+    while !window.should_close() {
+        // events
+        // -----
+        process_events(&mut window, &events, &mut mix_value);
+
+		unsafe {
+			gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+			gl::Clear(gl::COLOR_BUFFER_BIT);
+
+			// render the triangle
+			gl::ActiveTexture(gl::TEXTURE0);
+			gl::BindTexture(gl::TEXTURE_2D, texture1);
+			gl::ActiveTexture(gl::TEXTURE1);
+			gl::BindTexture(gl::TEXTURE_2D, texture2);
+			let offset_cstring = CString::new("mixValue").unwrap();
+			let offset_cstr = CStr::from_bytes_with_nul(offset_cstring.as_bytes_with_nul())
+				.expect("CStr conversion failed");
+
+			our_shader.set_float(offset_cstr, mix_value);
+
+			// create transformations
+			let mut transform: Matrix4<f32> = Matrix4::identity();
+			transform = transform * Matrix4::<f32>::from_translation(vec3(0.5, -0.5, 0.0));
+			transform = transform * Matrix4::<f32>::from_angle_z(Rad(glfw.get_time() as f32));
+
+			// get matrix's uniform location and set matrix
+			our_shader.use_program();
+			our_shader.set_mat4(CStr::from_bytes_with_nul(b"transform\0").unwrap(), &transform);
+
+
+			// seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+			gl::BindVertexArray(vao);
+
+			gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
+
+			transform = Matrix4::identity();
+			transform = transform * Matrix4::<f32>::from_translation(vec3(-0.5, 0.5, 0.0));
+			transform = transform * Matrix4::<f32>::from_scale(f32::sin(glfw.get_time() as f32));
+
+			our_shader.set_mat4(CStr::from_bytes_with_nul(b"transform\0").unwrap(), &transform);
+
+			gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
+        }
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        window.swap_buffers();
+        glfw.poll_events();
+    }
+}
+
+// NOTE: not the same version as in common.rs!
+fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::WindowEvent)>, mix_value: &mut f32) {
+    for (_, event) in glfw::flush_messages(events) {
+        match event {
+            glfw::WindowEvent::FramebufferSize(width, height) => {
+                // make sure the viewport matches the new window dimensions; note that width and
+                // height will be significantly larger than specified on retina displays.
+                unsafe { gl::Viewport(0, 0, width, height) }
+            }
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                println!("inside close");
+                window.set_should_close(true);
+            }
+			glfw::WindowEvent::Key(Key::Up, _, Action::Repeat, _) => {
+				println!("Pressing up");
+				*mix_value += 0.01;
+				if *mix_value >= 1.0 {
+					*mix_value = 1.0;
+				}
+			}
+			glfw::WindowEvent::Key(Key::Down, _, Action::Repeat, _) => {
+				println!("Pressing down");
+				*mix_value -= 0.01;
+				if *mix_value <= 0.0 {
+					*mix_value = 0.0;
+				}
+			}
+            _ => {}
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,9 @@ fn main() {
 		#[cfg(feature = "chapter-1")] "1_4_4" => main_1_4_4(),
 		#[cfg(feature = "chapter-1")] "1_4_5" => main_1_4_5(),
 		#[cfg(feature = "chapter-1")] "1_4_6" => main_1_4_6(),
+		#[cfg(feature = "chapter-1")] "1_5_1" => main_1_5_1(),
+		#[cfg(feature = "chapter-1")] "1_5_2" => main_1_5_2(),
+		#[cfg(feature = "chapter-1")] "1_5_3" => main_1_5_3(),
         _     => println!("Unknown tutorial id")
     }
 }
